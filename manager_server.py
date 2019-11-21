d1ff55ec19f81f20e6123874ff3ea746efccf651
rvsharma-cmu <39546440+rvsharma-cmu@users.noreply.github.com>
2019-11-21 04:46:11 -0500

    added container launch commands and few other much needed variables. ToDo: container kill and chroot for process spawning

--- manager_server.py
+++ manager_server.py
@@ -1,14 +1,35 @@
+from multiprocessing import Process
+
 import flask
 import sys
 from flask import request, jsonify, Response
 import os
+import os.path as ospath
 
 manager_server = flask.Flask(__name__)
 manager_server.config["DEBUG"] = True
 
-config_infor_list = []
+""" config information dictionary mapped to 
+    config information file name on disk 
+    caching on memory for faster access
+"""
+config_infor_dict = {}
+
 instances_list = []
 
+""" Current project working directory for hierarchically 
+    declaring paths and folders for files and basefs 
+"""
+proj_work_dir = ospath.dirname(ospath.realpath(__file__))
+
+""" Directory for container instances that mounts containers and 
+    its instances 
+"""
+cont_inst_dir = ospath.join(proj_work_dir, 'container')
+
+"""Dictionary of container processes 
+"""
+container_dict = {}
 
 @manager_server.route('/config', methods=['POST'])
 def create_config():
@@ -21,9 +42,9 @@ def create_config():
 
     if filename is None or major_version is None or minor_version is None:
         return Response(status=409)
-    global config_infor_list
+    global config_infor_dict
     config_file_name = filename + "-" + major_version + "-" + minor_version + ".cfg"
-    config_infor_list.append(config_file_name)
+    config_infor_dict[config_file_name] = content
     f = open(config_file_name, "w")
     f.write(content)
     f.close()
@@ -32,10 +53,44 @@ def create_config():
 
 @manager_server.route('/cfginfo', methods=['GET'])
 def get_config_info():
-    output_dict = {"files": config_infor_list}
+    result_list = []
+    for each_config_infor in config_infor_dict.keys():
+        result_list.append(each_config_infor)
+    output_dict = {"files": result_list}
     response = jsonify(output_dict)
     response.status_code = 200
     return response
+
+
+def do_mount(baseimg_dir, mount_point):
+    filename = mount_point.split(" ")[0]
+    file_folder = filename.split(".")[0]
+    folder = mount_point.split(" ")[1]
+    if folder[0] == '/':
+        folder = folder[1:]
+    permission = mount_point.split(" ")[2]
+    mount_dir = ospath.join(proj_work_dir, "mountables")
+    filepath = ospath.join(mount_dir, filename)
+    file_folder = ospath.join(mount_dir, file_folder)
+    folder_path = ospath.join(baseimg_dir, folder)
+    if not ospath.exists(folder_path):
+        os.system('sudo mkdir {}'.format(folder_path))
+    if not ospath.exists(file_folder):
+        os.system('tar -xf {} -C {}'.format(filepath, mount_dir))
+    if permission == "READ":
+        os.system('sudo mount --bind -o ro {} {}'.format(file_folder, folder_path))
+    else:
+        os.system('sudo mount --bind -o rw {} {}'.format(file_folder, folder_path))
+
+
+def start_container(instance_base_image_dir, config_information):
+    startup_env = config_information['startup_env']
+    if startup_env[-1] != ';':
+        startup_env = startup_env + ';'
+    os.system('unshare -p -f --mount-proc={} chroot {} /bin/bash -c "export {} {}"'.format(ospath.join(instance_base_image_dir, 'proc'),
+                                                                                           instance_base_image_dir, startup_env,
+                                                                                           config_information[
+                                                                                               'startup_script']))
 
 
 @manager_server.route('/launch', methods=['POST'])
@@ -44,31 +99,51 @@ def launch_inst():
     if content is None:
         return Response(status=409)
     
-    inst_name = content["name"]
-    inst_major = content["major"]
-    inst_minor = content["minor"]
-    if inst_name is None or inst_major is None or inst_minor is None:
+    config_name = content["name"]
+    major_version = content["major"]
+    minor_version = content["minor"]
+    if config_name is None or major_version is None or minor_version is None:
         return Response(status=409)
     global instances_list
-    for config_file in config_infor_list:
-        if config_file == inst_name:
-            # Code for launching instance of a specific container given
-            # inst_name, inst_major and inst_minor to be written here
 
-            
-            instance_name = "instance_" + inst_name
+    instance_name = "instance_" + config_name
+    instances_list.append(instance_name)
+
+    inst_dir = ospath.join(cont_inst_dir, instance_name)
+    if not ospath.exists(inst_dir):
+        os.makedirs(inst_dir, 777)
+    
+    os.system("tar -zxf base_images/basefs.tar.gz -C " + inst_dir + "")
+    instance_base_image_dir = ospath.join(inst_dir, 'basefs')
+    
+    config_information = config_infor_dict[config_name+"-"+major_version+"-"+minor_version+".cfg"]
+    
+    for each_mount_point in config_information['mounts']:
+        do_mount(instance_base_image_dir, each_mount_point)
+
+    os.system('mount -t proc proc {}'.format(ospath.join(instance_base_image_dir, 'proc')))
+    container_process = Process(target=start_container, args=(instance_base_image_dir, config_information))
+    container_dict[instance_name] = container_process
+    container_process.start()
+    # Set group id
+    os.setpgid(container_process.pid, container_process.pid)
+
+    for config_file in config_infor_dict.keys():
+        if config_file == config_name:
+            # Code for launching instance of a specific container given
+            # config_name, major_version and minor_version to be written here
+
+            instance_name = "instance_" + config_name
             instances_list.append(instance_name)
             output_dict = {}
             output_dict["instance"] = instance_name
-            output_dict["name"] = inst_name
-            output_dict["major"] = inst_major
-            output_dict["minor"] = inst_minor
+            output_dict["name"] = config_name
+            output_dict["major"] = major_version
+            output_dict["minor"] = minor_version
             response = jsonify(output_dict)
             response.status_code = 200
             return response 
     return Response(status=404)
-
-    
 
     
 @manager_server.route('/list', methods=['GET'])
@@ -96,8 +171,6 @@ def del_inst(text):
 @manager_server.route('/destroyall', methods=['DELETE'])
 def del_all_inst():
     global instances_list
-    for one_instance in instances_list:
-        # Code to kill instance to be written here
 
     
     instances_list.clear()
@@ -106,4 +179,4 @@ def del_all_inst():
 
 if __name__ == '__main__':
     # manager_server.run(host="localhost", port=int(sys.argv[2]))
-    manager_server.run(host="localhost", port=8080
+    manager_server.run(host="localhost", port=8080)
